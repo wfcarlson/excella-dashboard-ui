9e278e1ba401fb46a8cbe5e042b6744f73d28cb5
Tekle Ayele <tekleayele@gmail.com>
2018-09-19 16:38:11 -0400

    Simple Bar Chart

---
+++ src/app/Data.ts
@@ -0,0 +1,8 @@
+// Data.ts
+
+export class Data {
+    headCount : Number;
+    bdPipeline : Number;
+    recrutingPipeline : Number;
+    benchPluse : Number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Data } from './Data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'excella-dashboard-ui';
+
+  title = 'Excella Dashboard - Monthly Activity Report';
+  data = new Data;
+  url = 'http://10.68.245.220:8080/monthly-report/';
+  constructor(private httpClient: HttpClient) {}
+  chartData = [
+    { data: [], label: 'Headcount' },
+    { data: [], label: 'BD Pipeline' },
+    { data: [], label: 'Recruting Pipeline' },
+    { data: [], label: 'Bench+' }
+  ];
+  ngOnInit() {
+    this.httpClient.get(this.url).subscribe((res: Data) => {
+      this.data = res;
+      this.chartData = [
+        { data: [this.data.headCount], label: 'Headcount' },
+        { data: [this.data.bdPipeline], label: 'BD Pipeline' },
+        { data: [this.data.recrutingPipeline], label: 'Recruting Pipeline' },
+        { data: [this.data.benchPluse ], label: 'Bench+' }
+      ];
+      console.log(this.data);
+    });
+  }
+  chartOptions = {
+    responsive: true
+  };
+ 
+  
+   
+  chartLabels = ['September'];
+
+  onChartClick(event) {
+    console.log(event);
+  }
 }
